8bb93a127fc0eb4a25cf77bd4424fc5dace0aeea
Alexander Ljungberg Perme <alex.perme@gmail.com>
2017-10-16 20:34:49 +0200

    Completed user classes. TODO: write comments, minify after review.

--- php/userHandler.php
+++ php/userHandler.php
@@ -13,11 +13,30 @@
         }
 
         function login ($username, $password) {
-
+            if (!is_string($username) || !is_string($password)) { return false; }
+            $user = parent::read($username);
+            if (!$user || $user->locked != 0 || !password_verify($password, $user->hash))
+            { return false; }
+            return self::$token_handler->create($user);
         }
 
-        function logout () {
-            
+        function logout ($token) {
+            return self::$token_handler->destroy($token);
+        }
+
+        function changePassword ($token, $new_pass) {
+            if (!is_string($token) || !is_string($new_pass)) { return false; }
+            $user = self::$token_handler->verify($token);
+            if (!$user) { return false; }
+            $hash = password_hash($new_pass, PASSWORD_DEFAULT);
+            return parent::updateHash($user->username, $hash);
+        }
+
+        function changeDisplayName ($token, $new_name) {
+            if (!is_string($token) || !is_string($new_name)) { return false; }
+            $user = self::$token_handler->verify($token);
+            if (!$user) { return false; }
+            return parent::updateDisplayName($user->username, $new_name);
         }
 
     }
--- php/userModel.php
+++ php/userModel.php
@@ -5,8 +5,66 @@
 
     class userModel extends mysqlSocket {
 
-        protected function __construct () {
+        static private $query;
 
+        protected function __construct () {
+            parent::__construct();
+            self::$query->read = 'SELECT * FROM USERS WHERE USERNAME = ?';
+            self::$query->ud_hash = 'UPDATE USERS SET HASH = ? WHERE USERNAME = ?';
+            self::$query->ud_dname = 'UPDATE USERS SET DISPLAY_NAME = ? WHERE USERNAME = ?';
+        }
+
+        protected function read ($username) {
+            if (!is_string($username)) { return false; }
+            $conn = parent::connect();
+            if ($query = $conn->prepare(self::$query->read)) {
+                $query->bind_param('s', $username);
+                $query->execute();
+                $query->bind_result($username, $display_name, $hash, $type, $locked);
+                $query->fetch();
+                $data = (object)[
+                    'username' => $username,
+                    'display_name' => $display_name,
+                    'hash' => $hash,
+                    'type' => $type,
+                    'locked' => $locked
+                ];
+                $query->close();
+                $conn->close();
+                return $data->hash ? $data : false;
+            }
+            $conn->close();
+            return false;
+        }
+
+        protected function updateHash ($username, $newHash) {
+            if (!is_string($username) || !is_string($newHash)) { return false; }
+            $conn = parent::connect();
+            if ($query = $conn->prepare(self::$query->ud_hash)) {
+                $query->bind_param('ss', $newHash, $username);
+                $query->execute();
+                $result = $query->affected_rows > 0 ? true : false;
+                $query->close();
+                $conn->close();
+                return $result;
+            }
+            $conn->close();
+            return false;
+        }
+
+        protected function updateDisplayName ($username, $new_dname) {
+            if (!is_string($username) || !is_string($new_dname)) { return false; }
+            $conn = parent::connect();
+            if ($query = $conn->prepare(self::$query->ud_dname)) {
+                $query->bind_param('ss', $new_dname, $username);
+                $query->execute();
+                $result = $query->affected_rows > 0 ? true : false;
+                $query->close();
+                $conn->close();
+                return $result;
+            }
+            $conn->close();
+            return false;
         }
 
     }
